9eba5427191c429da3343fc1219afd68b01d6180
Zeeshan8220 <zeeshanism1@gmail.com>
2024-12-02 21:40:21 +0500

    Update lab task 5.py

--- Zeeshan Ahmad (125) AI lab tasks/lab task 5.py
+++ Zeeshan Ahmad (125) AI lab tasks/lab task 5.py
@@ -35,3 +35,75 @@ child1.add_child(child4)
 result = dfs_with_stack(root)
 print("DFS Traversal:", result)
 
+#2
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+def inorder(root):
+    result = []
+    stack = []
+    current = root
+
+    while True:
+        if current:
+            stack.append(current)
+            current = current.left
+        elif stack:
+            current = stack.pop()
+            result.append(current.value)
+            current = current.right
+        else:
+            break
+    
+    return result
+
+def preorder(root):
+    result = []
+    if root is None:
+        return result
+    stack = [root]
+
+    while stack:
+        node = stack.pop()
+        result.append(node.value)
+
+        if node.right:
+            stack.append(node.right)
+        if node.left:
+            stack.append(node.left)
+    
+    return result
+
+def postorder(root):
+    result = []
+    if root is None:
+        return result
+    stack = [root]
+    while stack:
+        node = stack.pop()
+        result.append(node.value)
+
+        if node.left:
+            stack.append(node.left)
+        if node.right:
+            stack.append(node.right)
+
+    return result[::-1]  
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+inorder_traversal = inorder(root)
+preorder_traversal = preorder(root)
+postorder_traversal = postorder(root)
+
+print("Inorder Traversal:", inorder_traversal)
+print("Preorder Traversal:", preorder_traversal)
+print("Postorder Traversal:", postorder_traversal)
+
